a2f211e35facff808205a5c6d830f315b2867ecd
高海洋 <gaohaiyang@juzifenqi.com>
2017-11-28 17:10:21 +0800

    1.delete old 2.add Basic libiary

--- app/src/main/java/com/company/myapp/MainActivity.java
+++ app/src/main/java/com/company/myapp/MainActivity.java
@@ -3,29 +3,16 @@ package com.company.myapp;
 import android.widget.Toast;
 
 import com.company.myapp.activity.ActivityStatusTestActivity;
-import com.company.myapp.activity.ApiRequestTestActivity;
 import com.company.myapp.activity.BannerTestActivity;
 import com.company.myapp.activity.DateTimeTestActivity;
 import com.company.myapp.activity.DialogTestActivity;
 import com.company.myapp.activity.FileDownloadTestActivity;
 import com.company.myapp.activity.FragmentStatusActivity;
 import com.company.myapp.activity.LabelViewTestActivity;
-import com.company.myapp.activity.ListViewMoreItemTypeTestActivity;
-import com.company.myapp.activity.ListViewTest1Activity;
-import com.company.myapp.activity.ListViewTest2Activity;
 import com.company.myapp.activity.LoadingTestActivity;
 import com.company.myapp.activity.MyDialogTestActivity;
 import com.company.myapp.activity.PaletteTestActivity;
 import com.company.myapp.activity.PictureTestActivity;
-import com.company.myapp.activity.PullZoomTest1Activity;
-import com.company.myapp.activity.RecyclerViewTest1Activity;
-import com.company.myapp.activity.RecyclerViewTest2Activity;
-import com.company.myapp.activity.RecyclerViewTest3Activity;
-import com.company.myapp.activity.RecyclerViewTest4Activity;
-import com.company.myapp.activity.RefreshTest1Activity;
-import com.company.myapp.activity.RefreshTest2Activity;
-import com.company.myapp.activity.RefreshTest3Activity;
-import com.company.myapp.activity.RefreshTest4Activity;
 import com.company.myapp.activity.SheetTestActivity;
 import com.company.myapp.activity.SlidingMenuTestActivity;
 import com.company.myapp.activity.SnackBarTestActivity;
@@ -106,11 +93,6 @@ public class MainActivity extends AbsBaseActivity {
         startActivity(this, FragmentStatusActivity.class);
     }
 
-    @OnClick(R.id.btn_test4)
-    public void btn4Click() {
-        startActivity(this, ApiRequestTestActivity.class);
-    }
-
     @OnClick(R.id.btn_pic_test)
     public void picTestClick() {
         startActivity(this, PictureTestActivity.class);
@@ -157,41 +139,6 @@ public class MainActivity extends AbsBaseActivity {
         startActivity(this, SheetTestActivity.class);
     }
 
-    @OnClick(R.id.btn_listView1)
-    public void btnListView1(){
-        startActivity(this, ListViewTest1Activity.class);
-    }
-
-    @OnClick(R.id.btn_listView2)
-    public void btnListView2(){
-        startActivity(this, ListViewTest2Activity.class);
-    }
-
-    @OnClick(R.id.btn_listView_item_type)
-    public void btnListViewItemType(){
-        startActivity(this, ListViewMoreItemTypeTestActivity.class);
-    }
-
-    @OnClick(R.id.btn_pull_to_refresh1)
-    public void btnPullToRefresh1(){
-        startActivity(this, RefreshTest1Activity.class);
-    }
-
-    @OnClick(R.id.btn_pull_to_refresh2)
-    public void btnPullToRefresh2(){
-        startActivity(this, RefreshTest2Activity.class);
-    }
-
-    @OnClick(R.id.btn_pull_to_refresh3)
-    public void btnPullToRefresh3(){
-        startActivity(this, RefreshTest3Activity.class);
-    }
-
-    @OnClick(R.id.btn_pull_to_refresh4)
-    public void btnPullToRefresh4(){
-        startActivity(this, RefreshTest4Activity.class);
-    }
-
     @OnClick(R.id.btn_tab_change_color)
     public void btnTabChange(){
         startActivity(this, IndexHomeActivity.class);
@@ -215,31 +162,6 @@ public class MainActivity extends AbsBaseActivity {
     @OnClick(R.id.btn_palette)
     public void btnPalette(){
         startActivity(this, PaletteTestActivity.class);
-    }
-
-    @OnClick(R.id.btn_recycler1)
-    public void btnRecycleView1(){
-        startActivity(this, RecyclerViewTest1Activity.class);
-    }
-
-    @OnClick(R.id.btn_recycler2)
-    public void btnRecycleView2(){
-        startActivity(this, RecyclerViewTest2Activity.class);
-    }
-
-    @OnClick(R.id.btn_recycler3)
-    public void btnRecycleView3(){
-        startActivity(this, RecyclerViewTest3Activity.class);
-    }
-
-    @OnClick(R.id.btn_recycler4)
-    public void btnRecycleView4(){
-        startActivity(this, RecyclerViewTest4Activity.class);
-    }
-
-    @OnClick(R.id.btn_zoom1)
-    public void btnPullZoom1(){
-        startActivity(this, PullZoomTest1Activity.class);
     }
 
     @OnClick(R.id.btn_file_download)
